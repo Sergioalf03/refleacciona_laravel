dd7bdb42178706318e383079c4d65b8f68ea1858
Sergioalf03 <sergioalf03@gmail.com>
2023-01-06 01:04:13 -0700

    Add: Token working

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,8 +17,11 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->string('phone_number');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->integer('status')->default(2);
+            $table->string('key');
             $table->rememberToken();
             $table->timestamps();
         });
--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Validation\ValidationException;
+use Illuminate\Support\Str;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +21,70 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::middleware('auth:sanctum')->post('/1', function(Request $request) {
+    return $request->user();
+});
+
+Route::post('/sanctum/token', function (Request $request) {
+    $request->validate([
+        'email' => 'required|email',
+        'password' => 'required',
+        'device_name' => 'required',
+    ]);
+
+    $user = User::where('email', $request->email)->first();
+
+    if (!$user || !Hash::check($request->password, $user->password)) {
+        throw ValidationException::withMessages([
+            'email' => ['Las credenciales son incorrectas'],
+        ]);
+    }
+
+    return response()->json([
+        'code' => 200,
+        'message' => 'Success',
+        'data' => [
+            'token' => $user->createToken($request->device_name),
+            'user_name' =>  $user->name,
+            'user_email' =>  $user->email,
+        ],
+    ], 200);
+});
+
+route::post('/register', function(Request $request) {
+    $userDb = new user;
+    $userResult = $userDb::where('email', $request['email'])
+        ->first();
+
+    if (isset($userResult)) {
+        return abort(409, 'El correo ya está registrado');
+    }
+
+    $randomString = Str::random(64);
+
+    $userResult = $userDb::create([
+        'name' => $request['name'],
+        'email' => $request['email'],
+        'phone_number' => $request['phone_number'],
+        'password' => Hash::make($request['password']),
+        'key' => Hash::make($randomString),
+        'img' => 'nop', // to save img
+    ]);
+
+    return response()->json([
+        'code' => 200,
+        'message' => 'Success',
+        'data' => [
+            'id' => $userResult['id'],
+        ],
+    ], 200);
+});
+
+// remove tokens
+
+// Revoke all tokens...
+// $user->tokens()->delete();
+
+// Revoke a specific token...
+// $user->tokens()->where('id', $tokenId)->delete();
